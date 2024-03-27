4b0a9569829e23856bc3e42dd623464e418d8e7e
AE06909 <wes.gonzales@elevancehealth.com>
2024-03-27 15:25:13 -0400

    API implementation.

--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -1,20 +1,20 @@
-import {Routes, Route} from "react-router-dom"
-import {MovieList, MovieDetail, Search, PageNotFound} from "../pages"
+import { Routes, Route } from "react-router-dom"
+import { MovieList, MovieDetail, Search, PageNotFound } from "../pages"
 
 import React from 'react'
 
 export const AllRoutes = () => {
   return (
     <div className="dark:bg-slate-800">
-        <Routes>
-            <Route path="/" element={<MovieList />} />S
-            <Route path="/movie/:id" element={<MovieDetail />} />S
-            <Route path="/movies/popular" element={<MovieList />} />S
-            <Route path="/movies/top" element={<MovieList />} />S
-            <Route path="/movies/upcoming" element={<MovieList />} />S
-            <Route path="search" element={<Search />} />S
-            <Route path="*" element={<PageNotFound />} />S
-        </Routes>
+      <Routes>
+        <Route path="/" element={<MovieList apiPath="movie/now_playing" />} />
+        <Route path="/movie/:id" element={<MovieDetail />} />S
+        <Route path="/movies/popular" element={<MovieList apiPath="movie/popular" />} />S
+        <Route path="/movies/top" element={<MovieList apiPath="movie/top_rated" />} />S
+        <Route path="/movies/upcoming" element={<MovieList apiPath="movie/upcoming" />} />S
+        <Route path="search" element={<Search />} />S
+        <Route path="*" element={<PageNotFound />} />S
+      </Routes>
     </div>
   )
 }
