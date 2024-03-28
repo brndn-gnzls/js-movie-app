4cdbed7cdfd5c945e19e9d03a7eb9acc1a8240f5
AE06909 <wes.gonzales@elevancehealth.com>
2024-03-28 15:49:06 -0400

    Search functionality.

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,9 +1,9 @@
 import { useEffect } from 'react'
 import { useState } from 'react'
 
-export const useFetch = (api) => {
+export const useFetch = (api, queryTerm="") => {
     const [data, setData] = useState([])
-    const url = `https://api.themoviedb.org/3/${api}?api_key=${process.env.REACT_APP_API_KEY}`
+    const url = `https://api.themoviedb.org/3/${api}?api_key=${process.env.REACT_APP_API_KEY}&query=${queryTerm}`
 
     useEffect(() => {
         async function fetchMovies() {
