4dd1fc5a4d7981d846ddd2a6ceffd6b4f94abe50
AE06909 <wes.gonzales@elevancehealth.com>
2024-04-02 14:49:12 -0400

    Final.

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PageNotFoundImage from '../assets/images/pagenotfound.png'
 import { Link } from 'react-router-dom'
 import { Button } from '../components/Button'
 
 export const PageNotFound = () => {
+
+    useEffect(() => {
+        document.title = `Page Not Found`
+    })
+
     return (
         <main>
             <section className='flex flex-col justify-center px-2'>
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,6 +1,7 @@
 import { Card } from '../components/Card'
 import { useFetch } from '../hooks/useFetch'
 import { useSearchParams } from 'react-router-dom'
+import { useTitle } from '../hooks/useTitle'
 
 export const Search = ({apiPath}) => {
     const [searchParams] = useSearchParams()
@@ -8,11 +9,13 @@ export const Search = ({apiPath}) => {
 
     const { data: movies } = useFetch(apiPath, queryTerm)
 
+    useTitle(`Search result for ${queryTerm}`)
+
     return (
         <main>
             <section className='py-7'>
                 <p className='text-3xl text-gray-700 dark:text-white'>
-                    {movies.length === 0 ? `No results found for '${queryTerm}'` : `Resfult for '${queryTerm}'`}
+                    {movies.length === 0 ? `No results found for '${queryTerm}'` : `Result for '${queryTerm}'`}
                 </p>
             </section>
             <section className='max-w-7xl mx-auto py-7'>
